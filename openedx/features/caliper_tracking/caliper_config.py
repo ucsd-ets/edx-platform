3de2231b81f0b63c712c1feac46f9b6a4650d120
arslanhashmi <m.arslanijaz9@gmail.com>
2018-10-29 15:58:15 +0500

    Basic structure for base transformer

--- openedx/features/caliper_tracking/caliper_config.py
+++ openedx/features/caliper_tracking/caliper_config.py
@@ -1,8 +1,10 @@
 from openedx.features.caliper_tracking.transformers.bookmark_transformers import edx_bookmark_listed
 
 
-"""Mapping of events to their transformer functions
 """
+Mapping of events to their transformer functions
+"""
+
 EVENT_MAPPING = {
     'edx.bookmark.listed': edx_bookmark_listed,
 }
