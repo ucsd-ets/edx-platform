ac645b6c7ccdf857aeff10041b016c3c87b02d55
Zainab Amir <zainab@A00702-Zainab-Amir.arbisoft.com>
2018-10-30 10:16:59 +0500

    basic structure for event: edx.course.enrollment.deactivated

--- openedx/features/caliper_tracking/transformers/enrollment_transformers.py
+++ openedx/features/caliper_tracking/transformers/enrollment_transformers.py
@@ -14,3 +14,21 @@ def edx_course_enrollment_activated(current_event, caliper_event):
     })
     caliper_event['actor']['type'] = 'Person'
     return caliper_event
+    
+
+def edx_course_enrollment_deactivated(current_event, caliper_event):
+    """
+    Caliper specific log for edX course unenrollment
+    """
+    caliper_object = {
+        'course_id': current_event['context']['course_id'],
+        'user_id': caliper_event['user_id'],
+        'mode': current_event['event']['mode']
+    }
+    caliper_event.update({
+        'object': caliper_object,
+        'type': 'Enrollment',
+        'action': 'Deactivated'
+    })
+    caliper_event['actor']['type'] = 'Person'
+    return caliper_event
--- openedx/features/caliper_tracking/utils.py
+++ openedx/features/caliper_tracking/utils.py
@@ -1,3 +1,11 @@
 """
 Utils required in transformers
 """
+from opaque_keys.edx.locator import CourseLocator
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+
+
+def get_course(course_key):
+    # TODO not finalized
+    course_id = CourseLocator.from_string(course_key)
+    return CourseOverview.objects.get(id=course_id)
