10f09ef3fc43e6d1e4a6fb0b369347daa18d0846
Muhammad Umar Khan <umar.khan@arbisoft.com>
2018-10-30 16:34:16 +0500

    Add transformer for edx.course.enrollment.activated event

--- openedx/features/caliper_tracking/caliper_config.py
+++ openedx/features/caliper_tracking/caliper_config.py
@@ -1,5 +1,5 @@
 from openedx.features.caliper_tracking.transformers.bookmark_transformers import edx_bookmark_listed
-
+from openedx.features.caliper_tracking.transformers.enrollment_transformers import edx_course_enrollment_activated
 
 """
 Mapping of events to their transformer functions
@@ -7,4 +7,5 @@ Mapping of events to their transformer functions
 
 EVENT_MAPPING = {
     'edx.bookmark.listed': edx_bookmark_listed,
+    'edx.course.enrollment.activated': edx_course_enrollment_activated,
 }
