8b8c28dae542a4344373a968f4142ec9a7e40a92
arslanhashmi <m.arslanijaz9@gmail.com>
2018-12-06 12:56:02 +0500

    Update base caliper transformer according to new standards set

--- openedx/features/caliper_tracking/base_transformer.py
+++ openedx/features/caliper_tracking/base_transformer.py
@@ -16,36 +16,34 @@ class CaliperBaseTransformer(object):
     This class is responsible for adding all the caliper compliant
     fields which are common to all events
     """
+
     def __init__(self, event):
         """Constructor
 
         @param event: unprocessed event dict
         """
         self.event = event
-        self.caliper_event = dict()
-        self.add_generic_fields()
-        self.add_actor_info()
+        self.caliper_event = {}
+        self._add_generic_fields()
+        self._add_actor_info()
+        self._add_referrer()
+        self._add_extensions()
 
-    def add_generic_fields(self):
+    def _add_generic_fields(self):
         """
         Adds all of the generic fields to the event object
         """
         self.caliper_event.update({
             '@context': CALIPER_EVENT_CONTEXT,
-            'id': str(uuid.uuid4().urn),
-            'agent': self.event.get('agent'),
-            'edx_event_type': self.event.get('event_type'),
-            'host': self.event.get('host'),
-            'session': self.event.get('session'),
-            'referrer': self.event.get('referer'),
-            'user_id': self.event['context'].get('user_id'),
-            'org_id': self.event['context'].get('org_id'),
-            'path': self.event['context'].get('path'),
-            'eventTime': datetime.now().isoformat('T')
+            'id': uuid.uuid4().urn,
+            'eventTime': datetime.now().isoformat('T')[:-3] + 'Z'
         })
 
-    def add_actor_info(self):
-        self.caliper_event['actor'] = dict()
+    def _add_actor_info(self):
+        """
+        Adds all generic information related to `actor`
+        """
+        self.caliper_event['actor'] = {}
         user_profile_link = '{lms_url}{profile_link}'.format(
             lms_url=settings.LMS_ROOT_URL,
             profile_link=str(reverse('learner_profile', kwargs={'username': self.event.get('username')}))
@@ -54,5 +52,35 @@ class CaliperBaseTransformer(object):
             'id': user_profile_link,
         })
 
+    def _add_extensions(self):
+        """
+        A map of additional attributes not defined by the model MAY be specified for a more concise representation of
+        the Event.
+        """
+        self.caliper_event['extensions'] = {}
+        self.caliper_event['extensions']['extra_fields'] = {
+            'agent': self.event.get('agent'),
+            'event_type': self.event.get('event_type'),
+            'host': self.event.get('host'),
+            'org_id': self.event['context'].get('org_id'),
+            'path': self.event['context'].get('path'),
+            'session': self.event.get('session'),
+            'user_id': self.event['context'].get('user_id'),
+            'accept_language': self.event.get('accept_language'),
+            'page': self.event.get('page'),
+        }
+
+    def _add_referrer(self):
+        """
+        Adds information of an Entity that represents the referring context.
+        """
+        self.caliper_event['referrer'] = {
+            'id': self.event.get('referer')
+        }
+
     def transform_event(self):
+        """
+        Provides Generic Caliper transformer event.
+        :return: Transformed event with basic Caliper standards.
+        """
         return self.caliper_event
