fd1b57279fb6bb7d59ea0a86bf25bb8bced3690a
Tehreem Sadat <tehreem.sadat@arbisoft.com>
2018-12-12 14:56:45 +0500

    Add Json for edx.forum.thread.created event

--- openedx/features/caliper_tracking/transformers/forum_transformers.py
+++ openedx/features/caliper_tracking/transformers/forum_transformers.py
@@ -1,5 +1,5 @@
 """
-Transformers for all the problems events
+Transformers for all the forums events
 """
 
 
@@ -37,5 +37,48 @@ def edx_forum_response_created(current_event, caliper_event):
         'course_id': current_event['context']['course_id'],
         'ip': current_event['ip']
     })
+
+    return caliper_event
+
+
+def edx_forum_thread_created(current_event, caliper_event):
+    """
+    Users create a new top-level thread, also known as a post, by clicking
+    New Post and then submitting their contributions. When these actions
+    are complete, the server emits an edx.forum.thread.created
+
+    :param current_event: default log
+    :param caliper_event: log containing both basic and default attribute
+    :return: final created log
+    """
+
+    current_event_details = current_event['event']
+
+    caliper_object = {
+        'body': current_event_details.pop('body', ''),
+        'id': current_event['referer'],
+        'type': 'Message',
+        'name': current_event_details.pop('title', ''),
+        'extensions': current_event_details
+    }
+
+    caliper_event.update({
+        'type': 'MessageEvent',
+        'action': 'Posted',
+        'object': caliper_object
+    })
+
+    caliper_event['extensions']['extra_fields'].update({
+        'ip': current_event['ip'],
+        'course_id': current_event['context']['course_id'],
+        'course_user_tags': current_event['context']['course_user_tags']
+    })
+
+    caliper_event['actor'].update({
+        'type': 'Person',
+        'name': current_event['username']
+    })
+
+    caliper_event['referrer']['type'] = 'WebPage'
 
     return caliper_event
