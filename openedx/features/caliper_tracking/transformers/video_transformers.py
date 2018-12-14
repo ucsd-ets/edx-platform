b3d6570395773104bc1842b68aaeff9a7095aee0
Hassan Tariq <hassan@Hassan-Tariq-A00053.arbisoft.com>
2018-12-14 15:09:38 +0500

    Transformed Event "load_video" add code and JSON

--- openedx/features/caliper_tracking/transformers/video_transformers.py
+++ openedx/features/caliper_tracking/transformers/video_transformers.py
@@ -9,10 +9,11 @@ from isodate import duration_isoformat
 
 def pause_video(current_event, caliper_event):
     """
-    When a user selects the video player's pause control, the player emits
-    a pause_video event. Fot videos that are streamed in a browser, when
-    the player reaches the end of the video file and play automatically
-    stops it emits both this event and a stop event (as of June 2014).
+    When a user selects the video player's pause control,
+    the player emits a pause_video event. For videos that
+    are streamed in a browser, when the player reaches the
+    end of the video file and play automatically stops it
+    emits both this event and a stop event (as of June 2014).
 
     :param current_event: default event log generated.
     :param caliper_event: caliper_event log having some basic attributes.
@@ -23,8 +24,9 @@ def pause_video(current_event, caliper_event):
         'action': 'Paused',
         'type': 'MediaEvent',
         'object': {
-            'duration': duration_isoformat(
-                timedelta(seconds=current_event_details['duration'])),
+            'duration': duration_isoformat(timedelta(
+                seconds=current_event_details['duration']
+            )),
             'extensions': {
                 'code': current_event_details['code'],
                 'id': current_event_details['id']
@@ -33,8 +35,9 @@ def pause_video(current_event, caliper_event):
             'type': 'VideoObject'
         },
         'target': {
-            'currentTime': duration_isoformat(
-                timedelta(seconds=current_event_details['currentTime'])),
+            'currentTime': duration_isoformat(timedelta(
+                seconds=current_event_details['currentTime']
+            )),
             'id': current_event['referer'],
             'type': 'MediaLocation'
         }
@@ -114,8 +117,51 @@ def edx_video_speed_changed(current_event, caliper_event):
     caliper_event['object'] = {
         'id': current_event.get('referer'),
         'type': 'VideoObject',
-        'duration': duration_isoformat(
-            timedelta(seconds=event_info.pop('duration'))),
+        'duration': duration_isoformat(timedelta(seconds=event_info.pop('duration'))),
         'extensions': event_info
     }
+    return caliper_event
+
+
+def load_video(current_event, caliper_event):
+    """
+    When the video is fully rendered and ready to play,
+    the browser or mobile app emits a load_video event.
+
+
+    :param current_event: default log
+    :param caliper_event: log containing both basic and default attribute
+    :return: final created log
+    """
+    caliper_event.update({
+        'action': 'Retrieved',
+        'type': 'Event'
+    })
+
+    caliper_event['extensions']['extra_fields'].update({
+        'course_id': current_event['context'].get('course_id'),
+        'ip': current_event.get('ip')
+    })
+
+    caliper_event['referrer']['type'] = 'WebPage'
+
+    caliper_event['actor'].update({
+        'name': current_event.get('username'),
+        'type': 'Person'
+    })
+
+    event_info = json.loads(current_event['event'])
+
+    caliper_event['object'] = {
+        'id': current_event.get('referer'),
+        'type': 'VideoObject',
+        'duration': duration_isoformat(timedelta(
+            seconds=event_info.pop('duration')
+        )),
+        'extensions': {
+            'id': event_info.get('id'),
+            'code': event_info.get('code'),
+        }
+    }
+
     return caliper_event
