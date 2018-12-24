d0fce3d8d0705b801c2e1a89b9dded7aa8dd068e
Hassan Tariq <hassan@Hassan-Tariq-A00053.arbisoft.com>
2018-12-24 16:49:56 +0500

    Transform Event "oppia.exploration.state.changed" add code and json

--- openedx/features/caliper_tracking/transformers/third_party_transformers.py
+++ openedx/features/caliper_tracking/transformers/third_party_transformers.py
@@ -72,3 +72,40 @@ def edx_googlecomponent_document_displayed(current_event, caliper_event):
     caliper_event['extensions']['extra_fields'].pop('session')
     caliper_event['referrer']['type'] = 'WebPage'
     return caliper_event
+
+
+def oppia_exploration_state_changed(current_event, caliper_event):
+    """
+    The server emits an oppia.exploration.state.changed event when
+    a user interacts with an Oppia exploration component by submitting
+    an answer. Answers are not incorrect or correct.
+    All answer submissions change the state of the exploration.
+
+    :param current_event: default event log generated.
+    :param caliper_event: caliper_event log having some basic attributes.
+    :return: updated caliper_event.
+    """
+    caliper_object = {
+        'id': current_event['referer'],
+        'type': 'AssessmentItem',
+        'extensions': current_event['event']
+    }
+    caliper_event['extensions']['extra_fields'].update({
+        'ip': current_event['ip'],
+        'course_id': current_event['context']['course_id'],
+        'module': current_event['context']['module'],
+        'course_user_tags': current_event['context']['course_user_tags'],
+        'asides': current_event['context']['asides']
+    })
+    caliper_event.update({
+        'type': 'Event',
+        'action': 'Modified',
+        'object': caliper_object
+    })
+    caliper_event['actor'].update({
+        'type': 'Person',
+        'name': current_event['username']
+    })
+    caliper_event['extensions']['extra_fields'].pop('session')
+    caliper_event['referrer']['type'] = 'WebPage'
+    return caliper_event
