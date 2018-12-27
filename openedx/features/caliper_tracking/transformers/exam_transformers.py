1afe278f1e66fcf92097be5d767da25af2056170
Muhammad Umar Khan <umar.khan@arbisoft.com>
2018-12-27 19:59:18 +0500

    Add code and json files for the event

--- openedx/features/caliper_tracking/transformers/exam_transformers.py
+++ openedx/features/caliper_tracking/transformers/exam_transformers.py
@@ -405,3 +405,34 @@ def edx_special_exam_timed_created(current_event, caliper_event):
     caliper_event['referrer']['type'] = 'WebPage'
 
     return caliper_event
+
+
+def edx_special_exam_time_attempt_ready_to_submit(current_event, caliper_event):
+    """
+    The server emits this event when a learner has completed a proctored exam
+    and is prompted to submit the exam.
+
+    :param current_event: default event log generated.
+    :param caliper_event: caliper_event log having some basic attributes.
+    :return: updated caliper_event.
+    """
+    caliper_event.update({
+        'type': 'NavigationEvent',
+        'action': 'NavigatedTo',
+        'object': {
+            'id': current_event['referer'],
+            'type': 'WebPage',
+            'name': current_event['event'].pop('exam_name'),
+            'extensions': current_event['event']
+        }
+    })
+    caliper_event['extensions']['extra_fields'].update({
+        'course_id': current_event['context']['course_id'],
+        'ip': current_event['ip']
+    })
+    caliper_event['actor'].update({
+        'type': 'Person',
+        'name': current_event['username']
+    })
+    caliper_event['referrer']['type'] = 'WebPage'
+    return caliper_event
