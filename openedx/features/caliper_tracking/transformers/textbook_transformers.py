27a2dcd000d840b971794c8456b504ca4f32f750
Hamza Farooq <hamza.farooq@arbisoft.com>
2018-12-31 18:37:32 +0500

    Add current and expected jsons with transformer code for textbook.pdf.searchcasesensitivity.toggled

--- openedx/features/caliper_tracking/transformers/textbook_transformers.py
+++ openedx/features/caliper_tracking/transformers/textbook_transformers.py
@@ -273,3 +273,42 @@ def textbook_pdf_zoom_buttons_changed(current_event, caliper_event):
         'object': caliper_event_object
     })
     return caliper_event
+
+
+def textbook_pdf_searchcasesensitivity_toggled(current_event, caliper_event):
+    """
+    The browser emits textbook.pdf.searchcasesensitivity.toggled events when a
+    user selects or clears the Match Case option for a search.
+
+    :param current_event: default event log generated.
+    :param caliper_event: caliper_event log having some basic attributes.
+    :return: updated caliper_event.
+    """
+
+    caliper_event['actor'].update({
+        'type': 'Person',
+        'name': current_event['username']
+    })
+
+    caliper_event['referrer']['type'] = 'WebPage'
+
+    caliper_event['extensions']['extra_fields'].update({
+        'course_id': current_event['context'].get('course_id'),
+        'ip': current_event['ip'],
+    })
+
+    current_event_details = json.loads(current_event['event'])
+
+    caliper_event_object = {
+        'id': current_event['referer'],
+        'type': 'SoftwareApplication',
+        'extensions': current_event_details
+    }
+
+    caliper_event.update({
+        'action': 'Used',
+        'type': 'ToolUseEvent',
+        'object': caliper_event_object
+    })
+
+    return caliper_event
