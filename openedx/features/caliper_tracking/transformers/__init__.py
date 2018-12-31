dea83b9060ff17227b64f073937588df8d5ef72c
Hamza Farooq <hamza.farooq@arbisoft.com>
2018-12-31 18:07:44 +0500

    Add current and expected jsons with transformer for textbook.pdf.search.highlight.toggled

--- openedx/features/caliper_tracking/transformers/__init__.py
+++ openedx/features/caliper_tracking/transformers/__init__.py
@@ -80,6 +80,7 @@ from .textbook_transformers import (
     textbook_pdf_zoom_buttons_changed,
     book,
     textbook_pdf_searchcasesensitivity_toggled,
+    textbook_pdf_search_highlight_toggled,
 )
 from .notes_transformers import (
     edx_course_student_notes_added,
--- openedx/features/caliper_tracking/transformers/textbook_transformers.py
+++ openedx/features/caliper_tracking/transformers/textbook_transformers.py
@@ -110,8 +110,8 @@ def textbook_pdf_page_navigated(current_event, caliper_event):
         'id': current_event['referer'],
         'type': 'Document',
         'extensions': {
-                'chapter': current_event_details['chapter'],
-                'page': current_event_details['page']
+            'chapter': current_event_details['chapter'],
+            'page': current_event_details['page']
         }
     }
 
@@ -196,7 +196,7 @@ def book(current_event, caliper_event):
         'type': 'NavigationEvent',
         'object': caliper_event_object
     })
-    
+
     return caliper_event
 
 
@@ -279,6 +279,45 @@ def textbook_pdf_searchcasesensitivity_toggled(current_event, caliper_event):
     """
     The browser emits textbook.pdf.searchcasesensitivity.toggled events when a
     user selects or clears the Match Case option for a search.
+
+    :param current_event: default event log generated.
+    :param caliper_event: caliper_event log having some basic attributes.
+    :return: updated caliper_event.
+    """
+
+    caliper_event['actor'].update({
+        'type': 'Person',
+        'name': current_event['username']
+    })
+
+    caliper_event['referrer']['type'] = 'WebPage'
+
+    caliper_event['extensions']['extra_fields'].update({
+        'course_id': current_event['context'].get('course_id'),
+        'ip': current_event['ip'],
+    })
+
+    current_event_details = json.loads(current_event['event'])
+
+    caliper_event_object = {
+        'id': current_event['referer'],
+        'type': 'SoftwareApplication',
+        'extensions': current_event_details
+    }
+
+    caliper_event.update({
+        'action': 'Used',
+        'type': 'ToolUseEvent',
+        'object': caliper_event_object
+    })
+
+    return caliper_event
+
+
+def textbook_pdf_search_highlight_toggled(current_event, caliper_event):
+    """
+    The browser emits textbook.pdf.search.highlight.toggled events when a user
+    selects or clears the Highlight All option for a search.
 
     :param current_event: default event log generated.
     :param caliper_event: caliper_event log having some basic attributes.
